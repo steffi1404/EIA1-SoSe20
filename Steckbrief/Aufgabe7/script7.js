102442dbc02c28d51ce8249fcbdc51046972ef7e
steffi1404 <64137935+steffi1404@users.noreply.github.com>
2020-06-15 13:21:29 +0200

    neu

--- Steckbrief/Aufgabe7/script7.js
+++ Steckbrief/Aufgabe7/script7.js
@@ -2,54 +2,35 @@
 //*TEIL1---------------------------------------------------------------------------------------------------------------------------------------*//
 window.addEventListener("load", function () {
     //*SOUNDS--------------------------------------------------------------------------------------------------------------------------------------*//
-    var sound0 = new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/A.mp3");
-    var sound1 = new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/C.mp3");
-    var sound2 = new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/F.mp3");
-    var sound3 = new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/G.mp3");
-    var sound4 = new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/hihat.mp3");
-    var sound5 = new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/kick.mp3");
-    var sound6 = new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/laugh-1.mp3");
-    var sound7 = new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/laugh-2.mp3");
-    var sound8 = new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/snare.mp3");
-    //*GESAMT--------------------------------------------------------------------------------------------------------------------------------------*//
-    // tslint:disable-next-line: typedef
-    var sound = [sound0, sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8];
+    var sounds = [];
+    sounds.push(new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/A.mp3"));
+    sounds.push(new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/C.mp3"));
+    sounds.push(new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/F.mp3"));
+    sounds.push(new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/G.mp3"));
+    sounds.push(new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/hihat.mp3"));
+    sounds.push(new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/kick.mp3"));
+    sounds.push(new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/laugh-1.mp3"));
+    sounds.push(new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/laugh-2.mp3"));
+    sounds.push(new Audio("https://raw.githubusercontent.com/steffi1404/EIA1-SoSe20/master/Steckbrief/Audio/snare.mp3"));
     //*ZENTRALE FUNKTION---------------------------------------------------------------------------------------------------------------------------*//
-    // tslint:disable-next-line: typedef
-    function playSample(sound) {
-        sound.play();
+    function playSample(samplenumber) {
+        sounds[samplenumber].play();
     }
-    //*TEIL2--------------------------------------------------------------------------------------------------------------------------------------*//
-    // tslint:disable-next-line: typedef
-    var beat = [sound5, sound8, sound4];
-    // tslint:disable-next-line: typedef
-    function play1() {
-        beat[0].play();
-    }
-    // tslint:disable-next-line: typedef
-    function play2() {
-        beat[1].play();
-    }
-    // tslint:disable-next-line: typedef
-    function play3() {
-        beat[2].play();
-    }
-    // tslint:disable-next-line: typedef
-    function play() {
-        setTimeout(function () { play1(); }, 1);
-        setTimeout(function () { play2(); }, 500);
-        setTimeout(function () { play3(); }, 1000);
+    //*TEIL2---------------------------------------------------------------------------------------------------------------------------------------*//
+    //*Inspiriert von Annette----------------------------------------------------------------------------------------------------------------------*//
+    function Beat() {
+        setInterval(function () { playSample(4); playSample(5); playSample(8); }, 300);
     }
     //*EVENT LISTENER------------------------------------------------------------------------------------------------------------------------------*//
-    document.querySelector("#b1").addEventListener("click", function () { playSample(sound[0]); }); /*A*/
-    document.querySelector("#b2").addEventListener("click", function () { playSample(sound[1]); }); /*C*/
-    document.querySelector("#b3").addEventListener("click", function () { playSample(sound[2]); }); /*F*/
-    document.querySelector("#b4").addEventListener("click", function () { playSample(sound[3]); }); /*G*/
-    document.querySelector("#b5").addEventListener("click", function () { playSample(sound[4]); }); /*hihat*/
-    document.querySelector("#b6").addEventListener("click", function () { playSample(sound[5]); }); /*kick*/
-    document.querySelector("#b7").addEventListener("click", function () { playSample(sound[6]); }); /*laugh1*/
-    document.querySelector("#b8").addEventListener("click", function () { playSample(sound[7]); }); /*laugh2*/
-    document.querySelector("#b9").addEventListener("click", function () { playSample(sound[8]); }); /*snare*/
-    document.querySelector("#play").addEventListener("click", play);
+    document.querySelector("#b1")?.addEventListener("click", function () { playSample(0); });
+    document.querySelector("#b2")?.addEventListener("click", function () { playSample(1); });
+    document.querySelector("#b3")?.addEventListener("click", function () { playSample(2); });
+    document.querySelector("#b4")?.addEventListener("click", function () { playSample(3); });
+    document.querySelector("#b5")?.addEventListener("click", function () { playSample(4); });
+    document.querySelector("#b6")?.addEventListener("click", function () { playSample(5); });
+    document.querySelector("#b7")?.addEventListener("click", function () { playSample(6); });
+    document.querySelector("#b8")?.addEventListener("click", function () { playSample(7); });
+    document.querySelector("#b9")?.addEventListener("click", function () { playSample(8); });
+    document.querySelector("play").addEventListener("click", Beat);
 });
 //# sourceMappingURL=script7.js.map
